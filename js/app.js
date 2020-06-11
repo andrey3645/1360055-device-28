f230594d9b5702a7f06d3702c6a679d877d17a48
Bronepuh <darckday2007@mail.ru>
2020-06-11 15:28:32 +0300

    исправил валидность, доверстал слайды

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let sliderControls = document.querySelector('.slider-controls');
+
+let sliderButtons = document.querySelectorAll('.slider-button');
+
+// for(var sliderButton of sliderButtons){
+//     console.log(sliderButton);
+//     sliderButton.onclick = function() {
+//         sliderButton.classList.toggle('checked');
+//     }
+// }
+
+
+
+
+
