adb22352a1f8f6fbd6094c47f1667a1b86168019
Keks <keks@htmlacademy.ru>
2020-06-18 01:05:07 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/map.js
@@ -0,0 +1,37 @@
+var mapPopup = document.querySelector('.map-popup-wrapper');
+
+var mapPopupOpen = document.querySelector('.info__contacts-map');
+console.log(mapPopupOpen);
+
+var mapPopupClose = document.querySelector('button.map-close');
+
+mapPopupOpen.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add('popup--active');
+});
+
+mapPopupClose.addEventListener('click', function () {
+    mapPopup.classList.remove('popup--active');
+});
+
+// Эскейп
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains('popup--active')) {
+            evt.preventDefault();
+            mapPopup.classList.remove('popup--active');
+        }
+    }
+});
+
+// Пробел
+
+mapPopupOpen.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 32) {
+        if (!mapPopup.classList.contains('popup--active')) {
+            evt.preventDefault();
+            mapPopup.classList.add('popup--active');
+        }
+    }
+});
